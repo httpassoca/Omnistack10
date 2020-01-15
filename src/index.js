7343d6ec902840bfd404b94068a8a1ac64320abc
Rafael Freitas <49541181+Rafonel@users.noreply.github.com>
2020-01-15 18:27:40 -0300

    Added two entries for the MongoDB
    When I switch my OS I have to alter the MongoDB adress, because my Windows use Node 2.2 and my Linux use the 3.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const mongoose = require('mongoose')
-const routes = require('./routes')
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 
-mongoose.connect('mongodb+srv://rafonel:rafonel@testepassoca-wplbk.gcp.mongodb.net/dbOmnistack10?retryWrites=true&w=majority',{
+// LINUX :
+// let mongoPort="mongodb+srv://rafonel:rafonel@testepassoca-wplbk.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// WINDOWS:
+let mongoPort =
+  "mongodb://rafonel:rafonel@testepassoca-shard-00-00-wplbk.gcp.mongodb.net:27017,testepassoca-shard-00-01-wplbk.gcp.mongodb.net:27017,testepassoca-shard-00-02-wplbk.gcp.mongodb.net:27017/test?ssl=true&replicaSet=TestePassoca-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+mongoose.connect(mongoPort, {
   useNewUrlParser: true,
-  useUnifiedTopology:true
-})
+  useUnifiedTopology: true
+});
 
 // parameters types
 // Query params> req.query(filtros, ordenação, paginação, GET)
@@ -18,4 +24,6 @@ mongoose.connect('mongodb+srv://rafonel:rafonel@testepassoca-wplbk.gcp.mongodb.n
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333,'', ()=>{console.log("Server is running on port 3333")});
+app.listen(3333, "", () => {
+  console.log("Server is running on port 3333");
+});
